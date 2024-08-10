8b5f7e59d9868b0f708a01a9cce0f9a8f3d9be7c
273* <keita.y310@icloud.com>
2024-08-11 00:44:00 +0900

    お気に入りのリスト表示を実装しました．

--- src/popup/components/Favorite/Favorite.tsx
+++ src/popup/components/Favorite/Favorite.tsx
@@ -1,7 +1,55 @@
+import { Separator } from "@/components/ui/separator";
+import { CircleX } from "lucide-react";
 import React from "react";
+import CopySelector from "../CopySelector";
 
 const Favorite = () => {
-  return <div>Favorite</div>;
+  let data_count = 14;
+
+  return (
+    <div className="w-full h-full">
+      {data_count >= 4 && <div className="h-[62px]"></div>}
+      <div className="bg-muted w-full rounded-md px-2 py-1">
+        <div className="w-full h-5 flex justify-between items-center">
+          <p className="text-xs tracking-wide text-emerald-400">
+            Favorites List
+          </p>
+        </div>
+
+        {Array.from({ length: data_count }).map((_, index) => (
+          // <div
+          //   key={index}
+          //   className="flex flex-col justify-center abg-muted abg-slate-600 w-full h-14"
+          // >
+          //   <div className="">
+          //     <p>title</p>
+          //     <p>yyyy / mm / dd</p>
+          //   </div>
+          //   <Separator />
+          // </div>
+          <div key={index}>
+            {index != 0 && <Separator className="my-2" />}
+            <div className="flex font-medium items-center justify-between">
+              <div className="flex items-center gap-2">
+                <CircleX className="h-[1.2rem] w-[1.2rem] hover:opacity-70 transition-all cursor-pointer text-destructive" />
+                <div>
+                  <p>test</p>
+                  <p className="font-inter text-muted-foreground">
+                    yyyy / mm / dd
+                  </p>
+                </div>
+              </div>
+              <div className="flex gap-2">
+                <div>y=x/2</div>
+                <CopySelector />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="h-[70px]"></div>
+    </div>
+  );
 };
 
 export default Favorite;
--- src/popup/components/Generate/Generate.tsx
+++ src/popup/components/Generate/Generate.tsx
@@ -1,7 +1,7 @@
 import { useTheme } from "@/components/theme-provider";
 import { Label } from "@/components/ui/label";
 import { Separator } from "@/components/ui/separator";
-import { ChevronDown, Star } from "lucide-react";
+import { Plus, Star } from "lucide-react";
 import React from "react";
 import TextareaAutosize from "react-autosize-textarea";
 import CopySelector from "../CopySelector";
@@ -9,14 +9,20 @@ import { TemplateSelector } from "./TemplateSelector";
 
 const Generate = () => {
   const { resolvedTheme } = useTheme();
+
   return (
-    <div className="sbg-slate-400 w-full h-full">
+    <div className="w-full h-full">
       <div className="bg-muted w-full h-32 rounded-md px-2 py-1">
         <div className="w-full h-5 flex justify-between items-center">
           <p className="text-xs tracking-wide text-emerald-400">Preview</p>
-          <Star className="h-[1.2rem] w-[1.2rem] mt-1 text-muted-foreground hover:opacity-70 transition-all cursor-pointer" />
+          {/* <Star className="h-[1.2rem] w-[1.2rem] mt-1 text-muted-foreground hover:opacity-70 transition-all cursor-pointer" /> */}
+          {/* <Star
+            fill="#34d399"
+            strokeWidth={0}
+            className="h-[1.2rem] w-[1.2rem] mt-1 text-muted-foreground hover:opacity-70 transition-all cursor-pointer"
+          /> */}
+          <Plus className="h-[1.2rem] w-[1.2rem] mt-1 text-muted-foreground hover:opacity-70 transition-all cursor-pointer" />
         </div>
-        {/* <Separator className={`${resolvedTheme == "dark" && "bg-zinc-700"} `} /> */}
         <div className="bg-muted w-full h-24"></div>
       </div>
       <div className="bg-muted w-full h-6 rounded-md mt-2">
@@ -27,7 +33,7 @@ const Generate = () => {
             placeholder="Enter Tex"
             maxRows={5}
           />
-          <CopySelector />
+          <CopySelector className="mx-2" />
         </div>
         <div>
           <p className="mt-4 text-lg">Template</p>
