5a47817edd27b482cf4fb43bfbe1a8a8310a77f0
273* <keita.y310@icloud.com>
2024-08-13 01:08:20 +0900

    設定の日本語に対応しました．

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,11 +1,4 @@
-import {
-  Moon,
-  Sun,
-  MonitorSmartphone,
-  Ellipsis,
-  SlidersHorizontal,
-  Settings,
-} from "lucide-react";
+import { Moon, Sun, MonitorSmartphone, Settings } from "lucide-react";
 import { useTheme } from "./theme-provider";
 import {
   DropdownMenu,
@@ -17,37 +10,16 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Switch } from "./ui/switch";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Label } from "./ui/label";
+
 import { useTranslation } from "react-i18next";
-
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-
-import enJson from "@/utils/locales/en.json";
-import jaJson from "@/utils/locales/ja.json";
-
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: enJson },
-    ja: { translation: jaJson },
-  },
-  // lng: "en",
-  // fallbackLng: "en",
-  // interpolation: { escapeValue: false },
-});
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
   const [t, i18n] = useTranslation();
 
-  const [lang, setLang] = useState("en");
   const [position, setPosition] = useState(theme);
-  console.log(lang);
-
-  useEffect(() => {
-    i18n.changeLanguage(lang);
-  }, [lang, i18n]);
 
   return (
     <DropdownMenu>
@@ -55,7 +27,7 @@ export function ModeToggle() {
         <Settings className="h-[1.2rem] w-[1.2rem] m-2 text-muted-foreground hover:opacity-70 transition-all cursor-pointer" />
       </DropdownMenuTrigger>
       <DropdownMenuContent className="font-inter border border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-        <DropdownMenuLabel>Theme</DropdownMenuLabel>
+        <DropdownMenuLabel>{t("テーマ")}</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
           <DropdownMenuRadioItem
@@ -64,7 +36,7 @@ export function ModeToggle() {
             className="cursor-pointer"
           >
             <Sun className="h-[1.2rem] w-[1.2rem] mr-2 hover:opacity-70 transition-all" />
-            Light
+            {t("ライト")}
           </DropdownMenuRadioItem>
           <DropdownMenuRadioItem
             value="dark"
@@ -72,7 +44,7 @@ export function ModeToggle() {
             className="cursor-pointer"
           >
             <Moon className="h-[1.2rem] w-[1.2rem] mr-2 hover:opacity-70 transition-all" />
-            Dark
+            {t("ダーク")}
           </DropdownMenuRadioItem>
           <DropdownMenuRadioItem
             value="system"
@@ -80,7 +52,7 @@ export function ModeToggle() {
             className="cursor-pointer"
           >
             <MonitorSmartphone className="h-[1.2rem] w-[1.2rem] mr-2 hover:opacity-70 transition-all" />
-            System
+            {t("システム")}
           </DropdownMenuRadioItem>
         </DropdownMenuRadioGroup>
         <DropdownMenuSeparator />
@@ -93,8 +65,10 @@ export function ModeToggle() {
           </Label>
           <Switch
             id="airplane-mode"
-            checked={lang === "ja"}
-            onCheckedChange={() => setLang(lang === "en" ? "ja" : "en")}
+            checked={i18n.language === "ja"}
+            onCheckedChange={() =>
+              i18n.changeLanguage(i18n.language === "en" ? "ja" : "en")
+            }
           />
         </div>
       </DropdownMenuContent>
