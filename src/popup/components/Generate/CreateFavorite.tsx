08017b9e5e475e236ba1cfd3609bf95958d85636
273* <keita.y310@icloud.com>
2024-08-15 20:18:01 +0900

    入力機能が完成しました．

--- src/popup/components/Generate/CreateFavorite.tsx
+++ src/popup/components/Generate/CreateFavorite.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import {
   Dialog,
   DialogContent,
@@ -12,14 +12,29 @@ import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useTranslation } from "react-i18next";
 import { MathJax, MathJaxContext } from "better-react-mathjax";
+import { DialogClose } from "@radix-ui/react-dialog";
 
 const CreateFavorite = ({ currentValue }: { currentValue: string }) => {
   const { t } = useTranslation();
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (ref.current?.value) {
+      console.log(ref.current.value);
+      console.log(currentValue);
+    }
+  };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Plus className="h-[1.2rem] w-[1.2rem] mt-1 text-muted-foreground hover:opacity-70 transition-all cursor-pointer" />
+        <Plus
+          className={`${
+            currentValue
+              ? "cursor-pointer"
+              : "pointer-events-none cursor-not-allowed"
+          } h-[1.2rem] w-[1.2rem] mt-1 text-muted-foreground hover:opacity-70 transition-all`}
+        />
       </DialogTrigger>
       <DialogContent className="p-3 w-10/12 font-inter rounded-md border border-border/100 bg-background/95 supports-[backdrop-filter]:bg-background/100">
         <DialogHeader className="space-y-0">
@@ -49,10 +64,13 @@ const CreateFavorite = ({ currentValue }: { currentValue: string }) => {
           <input
             className="outline-none bg-muted p-1 rounded-md font-light w-10/12 text-sm border"
             placeholder={t("タイトルを入力")}
+            ref={ref}
           />
-          <Button type="submit" className="h-7">
-            {t("追加")}
-          </Button>
+          <DialogClose asChild>
+            <Button type="submit" className="h-7" onClick={handleClick}>
+              {t("追加")}
+            </Button>
+          </DialogClose>
         </DialogFooter>
       </DialogContent>
     </Dialog>
