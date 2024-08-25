1631a03b673c6382e1c6aacddb99ab9bf5a05797
273* <keita.y310@icloud.com>
2024-08-25 16:24:41 +0900

    型エラーの修正とビルドを行いました．

--- src/redux/formulaSlice.ts
+++ src/redux/formulaSlice.ts
@@ -1,10 +1,11 @@
+import { FavoriteItemObj } from "@/utils/storage";
 import { createSlice } from "@reduxjs/toolkit";
 
 export const formulaSlice = createSlice({
   name: "formula",
   //   initialState,
   initialState: {
-    formula_list: [],
+    formula_list: [] as FavoriteItemObj[],
   },
   reducers: {
     setFormulaList: (state, action) => {
@@ -16,15 +17,15 @@ export const formulaSlice = createSlice({
     },
     updateFormula: (state, action) => {
       const index = state.formula_list.findIndex(
-        (item) => item.id === action.payload.id
+        (item: FavoriteItemObj) => item.id === action.payload.id
       );
       if (index !== -1) {
-        state.formula_list[index] = action.payload;
+        state.formula_list[index] = action.payload as FavoriteItemObj;
       }
     },
     removeFormula: (state, action) => {
       state.formula_list = state.formula_list.filter(
-        (item) => item.id !== action.payload
+        (item: FavoriteItemObj) => item.id !== action.payload
       );
     },
     clearFormulaList: (state) => {
